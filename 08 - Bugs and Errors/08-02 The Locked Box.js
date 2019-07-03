845af7df20ba84f443c54c31ab6ee7af369341ab
sheronW <sheronw1174@gmail.com>
2019-07-02 21:19:39 -0400

    8.2 finished(not sure if its accurate); ch. 8 finished

---
+++ 08 - Bugs and Errors/08-02 The Locked Box.js
@@ -0,0 +1,39 @@
+/* 8.2 The Locked Box
+    author: SheronW
+    date: 7/2/2019 */
+
+const box = {
+  locked: true,
+  unlock() { this.locked = false; },
+  lock() { this.locked = true;  },
+  _content: [],
+  get content() {
+    if (this.locked) throw new Error("Locked!");
+    return this._content;
+  }
+};
+
+function withBoxUnlocked(body) {
+  // Your code here.
+  box.unlock();
+  try{
+    return body();
+  }
+  finally{
+    box.lock();
+  }
+}
+
+withBoxUnlocked(function() {
+  box.content.push("gold piece");
+});
+
+try {
+  withBoxUnlocked(function() {
+    throw new Error("Pirates on the horizon! Abort!");
+  });
+} catch (e) {
+  console.log("Error raised: " + e);
+}
+console.log(box.locked);
+// → true
