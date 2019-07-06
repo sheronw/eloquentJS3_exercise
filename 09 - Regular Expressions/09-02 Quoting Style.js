4bbbdf4d2fec7772c967e749b9901640770f7d15
sheronW <sheronw1174@gmail.com>
2019-07-06 12:20:08 -0400

    9.2 finished; Groups that are not matched will be replaced by nothing.

---
+++ 09 - Regular Expressions/09-02 Quoting Style.js
@@ -0,0 +1,10 @@
+/* 9.1 Quoting Style
+    author: SheronW
+    date: 7/6/2019 */
+
+// Groups that are not matched will be replaced by nothing.
+
+    let text = "'I'm the cook,' he said, 'it's my job.'";
+    // Change this call.
+    console.log(text.replace(/(\W|^)'|(\W|$)/g, '$1"$2'));
+    // → "I'm the cook," he said, "it's my job."
