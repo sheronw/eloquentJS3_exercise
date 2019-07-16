42d578ce488cf34f3a4147159bf8d120ad215e3d
sheronW <sheronw1174@gmail.com>
2019-07-16 16:06:20 -0400

    ch. 12 special forms finished

--- 12 - Project - A Programming Language/12_language.js
+++ 12 - Project - A Programming Language/12_language.js
@@ -86,6 +86,36 @@ function evaluate(expr, scope){
 
 
 /* Special Forms */
+specialForms.if = (args, scope) => {
+  if(args.length!=3) throw new SyntaxError("Wrong Number of Arguments to if");
+  else if(evaluate(args[0], scope) !== false) return evaluate(args[1], scope);
+  else return evaluate(args[2], scope);
+}
+
+specialForms.while = (args, scope) =>{
+  if(args.length!=2) throw new SyntaxError("Wrong Number of Arguments to while");
+  while(evaluate(args[0],scope) !== false){
+    evaluate(args[1], scope);
+  }
+  return false;
+}
+
+specialForms.do = (args, scope) => {
+  let value = false;
+  for(let arg of args){
+    value = evaluate(arg, scope);
+  }
+  return value;
+}
+
+specialForms.define = (args, scope) => {
+  if(args.length!=2 || args[0].type!="word"){
+    throw new SyntaxError("Incorrect Use of define");
+  }
+  let value = evaluate(args[1], scope);
+  scope[args[0].name] = value;
+  return value;
+}
 
 
 /* The Environment */
