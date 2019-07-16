d35c7b5f35042b48224ef469d4f09f70ee854980
sheronW <sheronw1174@gmail.com>
2019-07-16 16:18:49 -0400

    ch.12 environment finished

--- 12 - Project - A Programming Language/12_language.js
+++ 12 - Project - A Programming Language/12_language.js
@@ -119,6 +119,22 @@ specialForms.define = (args, scope) => {
 
 
 /* The Environment */
+const topScope = Object.create(null);
 
+topScope.true = true;
+topScope.false = false;
+
+for(let op of ["+", "-", "*", "/", "==", "<", ">"]){
+  topScope[op] =  Function("a, b", `return a ${op} b;`);
+}
+
+topScope.print = value =>{
+  console.log(value);
+  return value;
+}
+
+function run(program){
+  return evaluate(parse(program), Object.create(topScope));
+}
 
 /* Functions */
