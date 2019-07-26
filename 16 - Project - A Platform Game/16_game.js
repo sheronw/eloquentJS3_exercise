41a14482473a761c26d666568d0c102f4bb4e40e
sheronW <sheronw1174@gmail.com>
2019-07-26 19:01:45 -0400

    ch.16 Actor Updates finished

--- 16 - Project - A Platform Game/16_game.js
+++ 16 - Project - A Platform Game/16_game.js
@@ -246,6 +246,53 @@ Coin.prototype.collide = function(state){
 }
 
 // Actor Updates
+Lava.prototype.updata = function(time, state) {
+  let newPos = this.pos.plus(this.speed.times(time));
+  if(!state.level.touches(newPos, this.size, "wall")){
+    return new Lava(newPos, this.speed, this.reset);
+  }
+  else if(this.reset){
+    return new Lava(this.reset, this.speed, this.reset);
+  }
+  else{
+    return new Lava(this.pos, this.speed.times(-1));
+  }
+};
+
+const wobbleSpeed  = 8, wobbleDist = 0.07;
+Coin.prototype.update = function(time) {
+  let wobble = this.wobble + time * wobbleSpeed;
+  let wobblePos = Math.sin(wobble) * wobbleDist;
+  return new Coin(this.basePos.plus(new Vec(0, wobblePos)),this.basePos,wobble);
+};
+
+const playerXSpeed = 7;
+const gravity = 30;
+const jumpSpeed = 17;
+
+Player.prototype.update = function(time, state, keys){
+  let xSpeed = 0;
+  if(keys.ArrowLeft) xSpeed += playerXSpeed;
+  if(keys.ArrowRight) xSpeed -= playerXSpeed;
+  let pos = this.pos;
+  let movedX = pos.plus(new Vec(time*xSpeed,0));
+  if(!state.level.touches(movedX, this.size, "wall")){
+    pos = movedX;
+  }
+
+  let ySpeed = this.speed.y + time*gravity;
+  let movedY = pos.plus(new Vec(0,time*ySpeed));
+  if(!state.level.touches(movedY, this.size, "wall")){
+    pos = movedY;
+  }
+  else if(keys.ArrowUp && ySpeed>0){
+    ySpeed = -jumpSpeed;
+  }
+  else{
+    ySpeed = 0;
+  }
+  return new Player(pos, new Vec(xSpeed, ySpeed));
+};
 
 // Tracking Keys
 
