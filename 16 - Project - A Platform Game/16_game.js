e86db35fa550e71e2e0df1a0227a7347d65844c1
sheronW <sheronw1174@gmail.com>
2019-07-26 19:18:48 -0400

    ch.16 Tracking Keys finished

--- 16 - Project - A Platform Game/16_game.js
+++ 16 - Project - A Platform Game/16_game.js
@@ -295,5 +295,19 @@ Player.prototype.update = function(time, state, keys){
 };
 
 // Tracking Keys
+function trackKeys(keys){
+  let down = Object.create(null);
+  function track(event){
+    if(keys.includes(event.key)){
+      down[event.key] = event.type=="keydown";
+      event.preventDefault();
+    }
+  }
+  window.addEventListener("keydown", track);
+  window.addEventListener("keyup", track);
+  return down;
+}
+
+const arrowKeys = trackKeys(["ArrowLeft","ArrowRight","ArrowUp"]);
 
 // Running the Game
