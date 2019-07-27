bf28dd897e47089b38b844c6c38ea23ec56488ef
sheronW <sheronw1174@gmail.com>
2019-07-26 20:13:39 -0400

    ch.16 Running the Game finished; Project finished

--- 16 - Project - A Platform Game/16_game.js
+++ 16 - Project - A Platform Game/16_game.js
@@ -311,3 +311,45 @@ function trackKeys(keys){
 const arrowKeys = trackKeys(["ArrowLeft","ArrowRight","ArrowUp"]);
 
 // Running the Game
+function runAnimation(frameFunc){
+  let lastTime = null;
+  function frame(time){
+    if(lastTime != null){
+      let timeStep = Math.min(100,time-lastTime)/1000;
+      if(frameFunc(timeStep)===false) return;
+    }
+    lastTime = time;
+    requestAnimationFrame(frame);
+  }
+  requestAnimationFrame(frame);
+}
+
+function runLevel(level, Display){
+  let display = new Display(document.body,level);
+  let state = State.start(level);
+  let ending = 1;
+  return new Promise(resolve => {
+    runAnimation(time => {
+      state = state.update(time, arrowKeys);
+      display.syncState(state);
+      if(state.status=="playing") return true;
+      else if(ending>0){
+         ending -= time;
+         return true;
+      }
+      else{
+        display.clear();
+        resolve(state.status);
+        return false;
+      }
+    });
+  });
+}
+
+async function runGame(plans, Display) {
+  for(let level = 0; level<plans.length){
+    let status = await runLevel(new Level(plans[level]), Display);
+    if(status == "won") level++;
+  }
+  console.log("You've won!");
+}
