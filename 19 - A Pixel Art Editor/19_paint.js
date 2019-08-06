a7251e5d1564882a5b5597d198ef44ac67c33e72
sheronW <sheronw1174@gmail.com>
2019-08-05 20:18:00 -0400

    ch.19 dom building finished

--- 19 - A Pixel Art Editor/19_paint.js
+++ 19 - A Pixel Art Editor/19_paint.js
@@ -33,6 +33,15 @@ function updateState(state, action){
 }
 
 //DOM Building
+function elt(type, props, ...children){
+  let dom = document.createElement(type);
+  if(props) Object.assign(dom, props);
+  for(let child of children){
+    if(typeof child != "string") dom.appendChild(child);
+    else dom.appendChild(document.createTextNode(child));
+  }
+  return dom;
+}
 
 // the Canvas
 
