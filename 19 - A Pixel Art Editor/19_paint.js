fd7c3a972baa3c2c2781991aff5486cb7cf510f2
sheronW <sheronw1174@gmail.com>
2019-08-06 11:55:13 -0400

    ch.19 the Canvas finished

--- 19 - A Pixel Art Editor/19_paint.js
+++ 19 - A Pixel Art Editor/19_paint.js
@@ -12,13 +12,16 @@ class Picture {
     this.height = height;
     this.pixels = pixels;
   }
+
   static empty(width, height, color){
     let pixels = new Array(width*height).fill(color);
     return new Picture(width, height, pixels);
   }
+
   pixel(x, y){
     return this.pixels[x+y*this.width];
   }
+
   draw(pixels){
     let copy = this.pixels.slice();
     for(let {x, y, color} of pixels){
@@ -44,6 +47,81 @@ function elt(type, props, ...children){
 }
 
 // the Canvas
+const scale = 10;
+
+class PictureCanvas{
+  constructor(picture, pointerDown){
+    this.dom = elt("canvas", {
+      onmousedown: event => this.mouse(event, pointerDown),
+      ontouchstart: event => this.touch(event, pointerDown)
+    });
+    this.syncState(picture);
+  }
+
+  syncState(picture){
+    if(this.picture == picture) return;
+    this.picture = picture;
+    drawPicture(this.picture, this.dom, scale);
+  }
+
+  mouse(downEvent, onDown){
+    if(downEvent.button !=0) return;
+    let pos = pointerPosition(downEvent, this.dom);
+    let onMove = onDown(pos);
+    if(!onMove) return;
+    let move = moveEvent => {
+      if(moveEvent.buttons ==0){
+        this.dom.removeEventListener("mousemove", move);
+      }
+      else{
+        let newPos = pointerPosition(moveEvent, this.dom);
+        if(newPos.x == pos.x && newPos.y == pos.y) return;
+        pos = newPos;
+        onMove(newPos);
+      }
+    };
+    this.dom.addEventListener("mousemove", move);
+  }
+
+  touch(startEvent, onDown){
+    let pos = pointerPosition(startEvent.touches[0], this.dom);
+    let onMove onDown(pos);
+    startEvent.preventDefault(); // prevent panning
+    if(!onMove) return;
+    let move = moveEvent => {
+      let newPos = pointerPosition(moveEvent.touches[0], this.dom);
+      if(newPos.x == pos.x && newPos.y == pos.y) return;
+      pos = newPos;
+      onMove(newPos);
+    };
+    let end = () => {
+      this.dom.removeEventListener("touchmove", move);
+      this.dom.removeEventListener("touchend", end);
+    }
+    this.dom.addEventListener("touchmove", move);
+    this.dom.addEventListener("touchend", end);
+  }
+
+}
+
+function drawPicture(picture, canvas, scale){
+  canvas.width = picture.width * scale;
+  canvas.height = picture.height * scale;
+  let cx = canvas.getContext("2d");
+
+  for(let y=0; y<picture.height; y++){
+    for(let x=0; x<picture.width; x++){
+      cx.fillStyle = picture.pixel(x, y);
+      cx.fillRect(x*scale, y*scale, scale, scale);
+    }
+  }
+}
+
+function pointerPosition(pos, domNode){
+  let rect = domNode.getBoundingClientRect();
+  return {x: Math.floor((pos.clientX-rect.left)/scale),
+              y: Math.floor((pos.clientY-rect.top)/scale)};
+}
 
 // the Application
 
