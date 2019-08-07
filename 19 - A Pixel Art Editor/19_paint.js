1faa7b9031435a5bbb80e6d3724cda2ee869347a
sheronW <sheronw1174@gmail.com>
2019-08-06 20:08:50 -0400

    ch.19 the Application finished

--- 19 - A Pixel Art Editor/19_paint.js
+++ 19 - A Pixel Art Editor/19_paint.js
@@ -124,6 +124,56 @@ function pointerPosition(pos, domNode){
 }
 
 // the Application
+class PixelEditor{
+  constructor(state, config){
+    let {tools, controls, dispatch} = config;
+    this.state = state;
+    this.canvas = new PictureCanvas(state.picture, pos => {
+      let tool = tools[this.state.tool];
+      let onMove = tool(pos, this.state, dispatch);
+      if(onMove) return pos => onMove(pos, this.state);
+    });
+    this.controls = controls.map(
+      Control => new Control(state, config));
+    this.dom = elt("div", {}, this.canvas.dom, elt("br"),
+                            ...this.controls.reduce(
+                              (accumulator, current) => accumulator.concat(" ", current.dom), []
+                            ));
+  }
+  syncState(state){
+    this.state = state;
+    this.canvas.synyState(state.picture);
+    for(let ctrl of this.controls) ctrl.syncState(state);
+  }
+}
+
+class ToolSelect {
+  contructor(state, {tools, dispatch}) {
+    this.select = elt("select", {
+      onchange: () => dispatch({tool: this.select.value})
+    }, ...Object.keys(tools).map(name => elt("option", {
+      selected: name == state.tool
+    }, name)));
+    this.dom = elt("label", null, "Tool: ", this.select);
+  }
+  syncState(state) {
+    this.select.value = state.tool;
+  }
+}
+
+class ColorSelect {
+  constructor(state, {dispatch}) {
+    this.input = elt("input", {
+      type: "color",
+      value: state.color;
+      onchange: () => dispatch({color: this.input.value})
+    });
+    this.dom = elt("label", null, "Color: ", this.input);
+  }
+  syncState(state) {
+    this.input.value = state.color;
+  }
+}
 
 // Drawing Tools
 
