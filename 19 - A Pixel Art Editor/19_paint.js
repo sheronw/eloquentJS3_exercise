433d2e248f81ab4ba800c37b3b372b5ed84cad32
sheronW <sheronw1174@gmail.com>
2019-08-19 16:12:28 -0400

    ch.19 Saving and Loading finished

--- 19 - A Pixel Art Editor/19_paint.js
+++ 19 - A Pixel Art Editor/19_paint.js
@@ -226,6 +226,78 @@ function pick(pos, state, dispatch){
 }
 
 // Saving and Loading
+class SaveButton {
+  constructor(state){
+    this.picture = state.picture;
+    this.button = elt("button", {onclick: ()=> this.save()}, "Save");
+  }
+  save(){
+    let canvas = elt("canvas");
+    drawPicture(this.picture, canvas, 1);
+    let link = elt("a", {
+      href: canvas.toDataURL(),
+      download: "pixelart.png"
+    });
+    document.body.appendChild(link);
+    link.click();
+    link.remove();
+  }
+  syncState(state){
+    this.picture = state.picture;
+  }
+}
+
+class LoadButton {
+  constructor(_, {dispatch}){
+    this.button = elt("button", {onclick: ()=> startLoad(dispatch)}, "Load");
+  }
+  syncState() {}
+}
+
+function startLoad(dispatch) {
+  let input = elt("input", {
+    type: "file",
+    onchange: () => finishLoad(input.files[0], dispatch)
+  });
+  document.body.appendChild(input);
+  input.click();
+  input.remove();
+}
+
+function finishLoad(file, dispatch){
+  if(file==null) return;
+  let reader = new FileReader();
+  reader.addEventListener("load", () => {
+    let image = elt("img", {
+      onload: () => dispatch({
+        picture: pictureFromImage(image)
+      });
+      src: reader.result
+    });
+  });
+  reader.readAsDataURL(file);
+}
+
+function pictureFromImage(image){
+  let width = Math.max(100, image.width);
+  let height = Math.max(100, image.height);
+  let pixels = [];
+  let canvas = elt("canvas", {width, height});
+  let cx = canvas.getContext("2d");
+  cx.drawImage(image,0,0);
+  let {data} = cx.getImageData(0, 0, width, height);
+
+  function hex(n) {
+    return n.toString(16).padStart(2, "0");
+  }
+
+  for(let i=0; i<data.length; i++){
+    let [r, g, b] = data.slice(i, i+3);
+    pixels.push("#" + hex(r) + hex(g) + hex(b));
+  }
+
+  return new Picture(width, height, pixels);
+}
 
 // Undo History
 
