3aa082ee1b9e0a2791b0e0176a2751bdc11aa0f7
sheronW <sheronw1174@gmail.com>
2019-08-19 18:37:59 -0400

    ch.19 Let's Draw finished

--- 19 - A Pixel Art Editor/19_paint.js
+++ 19 - A Pixel Art Editor/19_paint.js
@@ -335,3 +335,24 @@ function historyUpdateState(state, action) {
 }
 
 // Let's Draw
+const startState = {
+  tool: "draw";
+  color: "#000000",
+  picture: Picture.empty(60, 30, "#669999"),
+  done: [],
+  doneAt: 0
+};
+
+const baseTools = {draw, fill, rectangle, pick};
+
+const baseControls = [
+  ToolSelect, ColorSelect, SaveButton, LoadButton, UndoButton
+];
+
+function startPixelEditor({state=startState, tools=baseTools, controls=baseControls}){
+  let app = new PixelEditor(state, {tools, controls, dispatch(action){
+    state = historyUpdateState(state, action);
+    app.syncState(state);
+  }});
+  return app.dom;
+}
