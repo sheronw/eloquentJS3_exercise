d9e1a1b357f0c207262f13333ba5f0523791b95c
sheronW <sheronw1174@gmail.com>
2019-08-22 16:23:34 -0400

    19.4 finished; ch.19 finished

--- 19 - A Pixel Art Editor/19-04 Proper Lines.js
+++ 19 - A Pixel Art Editor/19-04 Proper Lines.js
@@ -4,8 +4,9 @@
 
     function draw(pos, state, dispatch) {
       function drawPixel({x, y}, state) {
-        let drawn = {x, y, color: state.color};
-        dispatch({picture: state.picture.draw([drawn])});
+        let drawn = drawLine(pos, {x, y}, state);
+        pos = {x, y};
+        dispatch({picture: state.picture.draw(drawn)});
       }
       drawPixel(pos, state);
       return drawPixel;
@@ -20,31 +21,24 @@
 
     function drawLine(start, end, state){
       let drawn = [];
-
-      function tinyLine(p1, p2){
-       if(p1.x == p2.x){ // horizontal
-         if(p1.y > p2.y) [p1, p2] = [p2, p1]; // make sure p1 with smaller y
-         for(let y = p1.y + 1; y < p2.y; y++){
-           drawn.push({x: p1.x, y, color: state.color});
-         }
-       }
-
-       else if(p1.y == p2.y){ // vertical
-         if(p1.x > p2.x) [p1, p2] = [p2, p1]; // make sure p1 with smaller x
-         for(let x = p1.x + 1; x < p2.x; x++){
-           drawn.push({x, y:p1.y, color: state.color});
-         }
-       }
-
-       else{ // cut into half
-         let x = Math.min(p1.x, p2.x) + Math.ceil(Math.abs(p1.x-p2.x)/2);
-         let y = Math.min(p1.y, p2.y) + Math.ceil(Math.abs(p1.y-p2.y)/2);
-         tinyLine(p1, {x, y});
-         tinyLine(p2, {x, y});
-       }
+      if(Math.abs(start.x - end.x) > Math.abs(start.y - end.y)){ // horizontal
+        if(start.x > end.x) [start, end] = [end, start]; // make sure to go from left to right
+        let deltaY = (start.y - end.y) / (start.x - end.x);
+        // loop coor x
+        for(let {x, y} = start; x <= end.x; x++){
+          drawn.push({x, y: Math.round(y), color: state.color});
+          y += deltaY;
+        }
       }
-
-      tinyLine(start, end);
+      else{ // vertical
+        if(start.y > end.y) [start, end] = [end, start]; // make sure to go from up to bottom
+        let deltaX = (start.x - end.x) / (start.y - end.y);
+        // loop coor y
+        for(let {x, y} = start; y <= end.y; y++){
+          drawn.push({x: Math.round(x), y, color: state.color});
+          x += deltaX;
+        }
+      }
       return drawn;
     }
 
